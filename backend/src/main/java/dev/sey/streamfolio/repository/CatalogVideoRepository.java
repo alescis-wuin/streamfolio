802b5038a479c874713bfc56b5f9c26ff57f48ab
Alexis <alexis.guinot@onsiea.com>
2026-06-13 18:56:32 +0200

    Preload video playback metadata

--- backend/src/main/java/dev/sey/streamfolio/repository/CatalogVideoRepository.java
+++ backend/src/main/java/dev/sey/streamfolio/repository/CatalogVideoRepository.java
@@ -1,7 +1,12 @@
 package dev.sey.streamfolio.repository;
 
 import dev.sey.streamfolio.domain.CatalogVideo;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CatalogVideoRepository extends JpaRepository<CatalogVideo, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"title", "title.genres"})
+    Optional<CatalogVideo> findById(Long id);
 }
