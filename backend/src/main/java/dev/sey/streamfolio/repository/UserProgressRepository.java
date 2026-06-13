81f37ba43f247a7de6017d2711787b0cff2f266c
Alexis <alexis.guinot@onsiea.com>
2026-06-13 18:56:46 +0200

    Preload progress videos

--- backend/src/main/java/dev/sey/streamfolio/repository/UserProgressRepository.java
+++ backend/src/main/java/dev/sey/streamfolio/repository/UserProgressRepository.java
@@ -5,9 +5,13 @@ import dev.sey.streamfolio.domain.UserAccount;
 import dev.sey.streamfolio.domain.UserProgress;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserProgressRepository extends JpaRepository<UserProgress, Long> {
+    @EntityGraph(attributePaths = {"video"})
     Optional<UserProgress> findByUserAndVideo(UserAccount user, CatalogVideo video);
+
+    @EntityGraph(attributePaths = {"video"})
     List<UserProgress> findByUser(UserAccount user);
 }
