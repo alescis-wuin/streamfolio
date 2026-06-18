bdabde2982db5571ed6c20aaf3f4e3db609a7d85
Alexis <alexis.guinot@onsiea.com>
2026-06-18 14:18:07 +0200

    perf(admin): enable database pagination for videos

--- backend/src/main/java/dev/sey/streamfolio/repository/CatalogVideoRepository.java
+++ backend/src/main/java/dev/sey/streamfolio/repository/CatalogVideoRepository.java
@@ -4,14 +4,22 @@ import dev.sey.streamfolio.domain.CatalogTitle;
 import dev.sey.streamfolio.domain.CatalogVideo;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-public interface CatalogVideoRepository extends JpaRepository<CatalogVideo, Long> {
+public interface CatalogVideoRepository extends JpaRepository<CatalogVideo, Long>, JpaSpecificationExecutor<CatalogVideo> {
     @Override
     @EntityGraph(attributePaths = {"title", "title.genres"})
     Optional<CatalogVideo> findById(Long id);
+
+    @Override
+    @EntityGraph(attributePaths = {"title", "title.genres"})
+    Page<CatalogVideo> findAll(Specification<CatalogVideo> specification, Pageable pageable);
 
     @EntityGraph(attributePaths = {"title", "title.genres"})
     @Query("select video from CatalogVideo video")
